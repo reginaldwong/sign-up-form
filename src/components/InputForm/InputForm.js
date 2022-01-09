4c261acd41dc0f8305c296b4d829b45f6e1972cf
Reginald Wong <reginald_wong@outlook.com>
2022-01-09 13:51:43 -0500

    add purple border on input focus

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './InputForm.css'
 
-function InputForm() {
+const InputForm = () => {
     return (
         <div className='input-container'>
-            <input type="text" className="first-name" placeholder='First Name'/>
-            <input type="text" className="last-name" placeholder='Last Name'/>
-            <input type="email" className="email" placeholder='Email Address'/>
-            <input type="password" className="password" placeholder='Password'/>
-            <button type="button" className="claim-button">Claim your free trial</button>
-            <p className='disclaimer'>By clicking the button, you are agreeing to our <span className="terms-services">Terms and Services</span></p>
+            <input type="text" className="first-name input-field" placeholder='First Name'/>
+            <input type="text" className="last-name input-field" placeholder='Last Name'/>
+            <input type="email" className="email input-field" placeholder='Email Address'/>
+            <input type="password" className="password input-field" placeholder='Password'/>
+            <button type="submit" className="claim-button input-field">Claim your free trial</button>
+            <p className='disclaimer'>By clicking the button, you are agreeing to our <a href="#" className="terms-services">Terms and Services</a></p>
         </div>
     )
 }
